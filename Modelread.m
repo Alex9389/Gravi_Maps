30161ba537c0415b1656aa83cc72de62eecfafed
Alexander <kolesnikov-23.09@bk.ru>
2016-11-28 11:15:27 +0300

    Add files via upload

--- Modelread.m
+++ Modelread.m
@@ -19,17 +19,25 @@
 
 function [Nmax,Ae,GM,C,S,dC,dS] = Modelread(filename)
 fid = fopen (filename ,'r');
-A1= fscanf (fid,'%g %g %g \n',3);
-Nmax=A1(1);
-Ae  =A1(2);
-GM  =A1(3);
 while (~feof(fid))
-  B=fscanf(fid,'%d %d %g %g %g %g \n',6);
-  n =B(1);
-  m =B(2);
-  C(n+1,m+1) =B(3);
-  S(n+1,m+1) =B(4);
-  dC(n+1,m+1)= B(5);
-  dS(n+1,m+1)= B(6);
+  search = fscanf (fid,'%s');
+  if (search == 'gfc')
+    B=fscanf(fid,'%d %d %g %g %g %g \n',6);
+	n =B(1);
+    m =B(2);
+    C(n+1,m+1) =B(3);
+    S(n+1,m+1) =B(4);
+    dC(n+1,m+1)= B(5);
+    dS(n+1,m+1)= B(6);
+  elseif (search == 'earth_gravity_constant')
+	GM = fscanf (fid,'%s');
+	%перевести в числа
+  elseif (search == 'radius')
+	Ae = fscanf (fid,'%s');
+	%перевести в числа
+  elseif (search == 'max_degree')
+	Nmax = fscanf (fid,'%s');
+	%перевести в числа
+  end
 end
 fclose(fid);
